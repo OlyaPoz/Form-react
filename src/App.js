c890bf3dce32b42dbc9e6c70209aadbd4b0a8b47
Olya <zaychenok37@gmail.com>
2021-03-14 15:31:27 +0200

    Add ToDoTask

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
-class App extends Component {
-  render () {
-    return (
-      <BrowserRouter>
+import React from 'react';
+//import { BrowserRouter, Switch, Route } from 'react-router-dom';
+//import Login from './pages/Login';
+//import SignUp from './pages/SignUp';
+//import Header from './components/Header';
+import ToDoPage from './pages/TaskToDo';
+
+const App = props => {
+  return (
+    /* <BrowserRouter>
       <Header />
         <Switch>
           <Route exact path='/'component={Login} />
           <Route path='/sign-up' component={SignUp} />
+          
         </Switch>
       </BrowserRouter>
-    );
-  }
-}
+*/
+    
+      <ToDoPage />
+    
+  );
+};
 
 export default App;
